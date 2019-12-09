8d5da745c2ca1eb7f7fdc1ef87f5a3389c8548e0
tungkraiwit <kraiwitkeyen1998@gmail.com>
2019-12-09 15:31:58 +0700

    customer and edit users

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,29 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('classicmodels.sqlite',sqlite3.OPEN_READONLY);
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 app.get('/', (req, res) => {
   // db.all() fetches all results from an SQL query into the 'rows' variable:
   db.all('SELECT *  FROM employees', (err, rows) => {
     // console.log(rows);
     // const allUsernames = rows.map(e => e.email);
-    // console.log(allUsernames);
+    // console.log(allUsernames);    
     res.send(rows);
-  });
+  });  
 });
+// app.post('/',(req,res) => {
+//   // res.send(req.body.userName+'   '+req.body.password)
+//   if(req.body.userName === "555"){
+//     const eieiV1 = {
+//       eieiLove : 55,
+//       krikri:99
+//     }
+//     res.send(eieiV1)
+//   }
+//   else{
+//     res.send("NAN")
+//   }
+// })
+
 
 module.exports = app;
